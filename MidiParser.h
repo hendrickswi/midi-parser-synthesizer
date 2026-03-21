c71a6300297cf58990e0e3af70949a53b87485b2
William Hendricks <whendricks6727@gmail.com>
2026-03-20 21:48:18 -0400

    Extended MidiParser.cpp functionality for reading 32 bit and 16 bit big endian values

--- MidiParser.h
+++ MidiParser.h
@@ -8,17 +8,31 @@
 
 
 class MidiParser {
+private:
+    MidiFile file;
+
+    // Header data:
+    // format--0 for single track, 1 for multi-track sync, 2 for multi-track async
+    uint16_t format = 0;
+    // number of tracks
+    uint16_t num_tracks = 0;
+    // ticks per quarter note
+    uint16_t division = 0;
+
+    // Current byte pos
+    std::size_t cursor = 0;
+
+    // Helper functions for Big-Endian format of midi files
+    uint16_t read_uint16();
+    uint32_t read_uint32();
+    std::string read_string(std::size_t length);
+
 public:
     MidiParser();
     MidiParser(const MidiFile& file);
     MidiParser(const MidiParser& other);
 
     bool parse();
-
-private:
-    MidiFile file;
-
-
 };
 
 
