9f8a47f6c667b811c27df5d9d0acb923a8a27710
hendrickswi <whendricks6727@gmail.com>
2026-05-04 15:34:58 -0400

    Standarized decorator convention for oscillators versus envelopes

---
+++ Synthesizer/Oscillators/Decorators/VibratoOscillator.h
@@ -0,0 +1,20 @@
+﻿#ifndef MIDI_PARSERSYNTHESIZER_VIBRATOOSCILLATOR_H
+#define MIDI_PARSERSYNTHESIZER_VIBRATOOSCILLATOR_H
+#include "OscillatorDecorator.h"
+
+class VibratoOscillator : public OscillatorDecorator {
+private:
+    double current_phase;
+    double phase_increment;
+
+    float base_hz;
+
+    float speed_hz;
+    float depth;
+
+public:
+    VibratoOscillator(std::unique_ptr<Oscillator> osc, double sample_rate = 44100.0f);
+
+};
+
+#endif //MIDI_PARSERSYNTHESIZER_VIBRATOOSCILLATOR_H
