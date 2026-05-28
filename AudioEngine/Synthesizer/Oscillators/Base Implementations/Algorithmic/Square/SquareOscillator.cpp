e9ab944a02d350249e3e227f43ab1c17edd10947
William Hendricks <whendricks6727@gmail.com>
2026-05-28 02:15:44 -0400

    Improved square oscillator playback, and filled some gm patches

--- AudioEngine/Synthesizer/Oscillators/Base Implementations/Algorithmic/Square/SquareOscillator.cpp
+++ AudioEngine/Synthesizer/Oscillators/Base Implementations/Algorithmic/Square/SquareOscillator.cpp
@@ -1,10 +1,6 @@
 #include "SquareOscillator.h"
 #include <cmath>
 
-inline int sign(const float val) {
-    return (0.0 < val) - (val < 0.0);
-}
-
 SquareOscillator::SquareOscillator() = default;
 
 SquareOscillator::SquareOscillator(float hz, float sample_rate)
@@ -20,29 +16,33 @@ float SquareOscillator::poly_blep(float t, float dt) {
     }
     else if (t > 1.0f - dt) {
         t = (t - 1.0f) / dt;
-        return t + t - t * t + 1.0f;
+        return t * t + t + t + 1.0f;
     }
     return 0.0f;
 }
 
 float SquareOscillator::get_sample() {
-    current_phase += phase_increment;
-    if (current_phase >= TWO_PI) {
-        current_phase -= TWO_PI;
-    }
-
     float normalized_phase = current_phase / TWO_PI;
     float dt = phase_increment / TWO_PI;
+    float raw_wave = (normalized_phase < 0.5f) ? 1.0f : -1.0f;
 
-    float raw_wave = sign(std::sin(current_phase));
-
+    // Upward transition at normalized phase 0.0
     float correction_up = poly_blep(normalized_phase, dt);
 
     float t_2 = normalized_phase + 0.5f;
     if (t_2 >= 1.0f) {
         t_2 -= 1.0f;
     }
+    // Downward transition at normalized phase 0.5
     float correction_down = poly_blep(t_2, dt);
 
-    return raw_wave + correction_up - correction_down;
+    // Increment logic
+    current_phase += phase_increment;
+    if (current_phase >= TWO_PI) {
+        current_phase -= TWO_PI;
+    }
+
+    // 1/sqrt(2) attenuation to align RMS power with other algorithmic oscillators
+    // Also prevents PolyBLEP overshoot causing clicks during playback
+    return raw_wave + correction_up - correction_down * 0.707f;
 }
